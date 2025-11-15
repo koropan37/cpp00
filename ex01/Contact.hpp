9c96b850c1cf7d2d020d8d0c29833a94aec712c9
koro <shunta37k@gmail.com>
2025-11-15 21:55:31 +0900

    ex01のファイルを作った

---
+++ ex01/Contact.hpp
@@ -0,0 +1,17 @@
+#ifndef CONTACT_NPP
+#define CONTACT_NPP
+
+#include <string>
+
+class Contact{
+	private:
+		std::string first_name_;
+		std::string last_name_;
+		std::string nickname_;
+		std::string phone_number_;
+		std::string darkest_secret_;
+	public:
+		
+};
+
+#endif
---
+++ ex01/PhoneBook.hpp
@@ -0,0 +1,15 @@
+#ifndef PHONE_BOOK_HPP
+#define PHONE_BOOK_HPP
+
+#include "Contact.hpp"
+
+#define PHONE_BOOK_SIZE
+class PhoneBook{
+	private:
+		Contact contacts_[PHONE_BOOK_SIZE];
+	public:
+		PhoneBook();
+
+};
+
+#endif
