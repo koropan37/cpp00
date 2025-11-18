9c08f3a598cdb44c92ad4e0156444038edac3d0d
koro <shunta37k@gmail.com>
2025-11-18 21:48:23 +0900

    11/18

---
+++ ex02/Account.cpp
@@ -0,0 +1,29 @@
+#include "Account.hpp"
+#include <iostream>
+#include <ctime>
+
+int Account::_nbAccounts = 0;
+int Account::_totalAmount = 0;
+int Account::_totalNbDeposits = 0;
+int Account::_totalNbWithdrawals = 0;
+
+Account::Account(int initial_deposit) {
+
+}
+
+Account::Account(void) {
+
+}
+
+Account::~Account(void) {
+
+}
+
+int Account::getNbAccounts(void) { return _nbAccounts; }
+int Account::getTotalAmount(void) { return _totalAmount; }
+int Account::getNbDeposits(void) { return _totalNbDeposits; }
+int Account::getNbWithdrawals(void) { return _totalNbWithdrawals; }
+
+void Account::displayAccountsInfos(void) {
+	
+}
